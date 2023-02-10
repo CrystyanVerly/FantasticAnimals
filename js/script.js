d45da9afccfe297bcea1eab277320c61904f9ac4
CrystyanVerly <crystyanknappverly@gmail.com>
2023-02-10 11:01:11 -0300

    finaliza animation scroll

--- js/script.js
+++ js/script.js
@@ -72,3 +72,27 @@ function initSmoothScroll() {
   });
 }
 initSmoothScroll();
+
+// scroll animation
+
+function initScrollAnimation() {
+  const _sections = document.querySelectorAll(".js-scroll");
+
+  if (_sections.length) {
+    const _calcWindowHeight = window.innerHeight * 0.6;
+
+    function scrollAnimation() {
+      _sections.forEach((section) => {
+        const _sectionTop = section.getBoundingClientRect().top;
+        const _isSectionVisible = _sectionTop - _calcWindowHeight < 0;
+
+        if (_isSectionVisible) {
+          section.classList.add("active");
+        }
+      });
+    }
+    scrollAnimation();
+    window.addEventListener("scroll", scrollAnimation);
+  }
+}
+initScrollAnimation();

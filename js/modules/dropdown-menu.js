146e90b57d010142be3bbbef7b51cfb7c2ab40b9
CrystyanVerly <crystyanknappverly@gmail.com>
2023-03-15 10:57:50 -0300

    adiciona menu drop down via js para touchs, ainda sem click outside

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,1 +1,15 @@
 export default function initMenuDropDown() {}
+
+const _dropDownMenus = document.querySelectorAll("[data-dropdown]");
+const _events = ["touchstart", "click"];
+
+function $handleClick(e) {
+  e.preventDefault();
+  this.classList.toggle("active");
+}
+
+_dropDownMenus.forEach((menu) => {
+  _events.forEach((eventUser) => {
+    menu.addEventListener(eventUser, $handleClick);
+  });
+});

757a7b13a505bdf2a533de9f01070d1c5887ce6f
Crystyan Knapp Verly <crystyanknappverly@gmail.com>
2023-04-25 09:59:32 -0300

    Accordion refatored

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import initTabNav from "./modules/tab-navigation.js";
-import initAccordionList from "./modules/accordion-list.js";
+import Accordion from "./modules/accordion-list.js";
 import SmoothScroll from "./modules/smooth-scroll.js";
 import initScrollAnimation from "./modules/animation-scroll.js";
 import initModal from "./modules/modal.js";
@@ -10,12 +10,13 @@ import initCompanyOperation from "./modules/companyOperation.js";
 import initFetchAnimals from "./modules/fetchAnimals.js";
 import initFetchBTC from "./modules/fetchBitCoin.js";
 
-initTabNav();
-initAccordionList();
-
 const smoothScroll = new SmoothScroll("[data-menu='link'] a[href^='#']");
 smoothScroll.init();
 
+const accordion = new Accordion("[data-anima='accordion'] dt");
+accordion.init();
+
+initTabNav();
 initScrollAnimation();
 initModal();
 initToolTip();

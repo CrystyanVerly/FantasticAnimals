b9a11f709e5854b1121d3eac02541c82833bfdf5
Crystyan Knapp Verly <crystyanknappverly@gmail.com>
2023-04-25 13:49:21 -0300

    toolTip refatored

--- js/script.js
+++ js/script.js
@@ -3,24 +3,24 @@ import Accordion from "./modules/accordion-list.js";
 import SmoothScroll from "./modules/smooth-scroll.js";
 import initScrollAnimation from "./modules/animation-scroll.js";
 import Modal from "./modules/modal.js";
-import initToolTip from "./modules/toolTip.js";
+import ToolTip from "./modules/toolTip.js";
 import initMenuDropDown from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menuMobile.js";
 import initCompanyOperation from "./modules/companyOperation.js";
 import initFetchAnimals from "./modules/fetchAnimals.js";
 import initFetchBTC from "./modules/fetchBitCoin.js";
 
-const smoothScroll = new SmoothScroll("[data-menu='link'] a[href^='#']");
-smoothScroll.init();
-
-const accordion = new Accordion("[data-anima='accordion'] dt");
-accordion.init();
-
 const tabNav = new TabNav(
   "[data-tab='menu'] li",
   "[data-tab='content'] section"
 );
 tabNav.init();
+
+const accordion = new Accordion("[data-anima='accordion'] dt");
+accordion.init();
+
+const smoothScroll = new SmoothScroll("[data-menu='link'] a[href^='#']");
+smoothScroll.init();
 
 const modal = new Modal(
   "[data-modal='open']",
@@ -29,8 +29,10 @@ const modal = new Modal(
 );
 modal.init();
 
+const toolTip = new ToolTip("[data-tooltip]");
+toolTip.init();
+
 initScrollAnimation();
-initToolTip();
 initMenuDropDown();
 initMenuMobile();
 initCompanyOperation();
